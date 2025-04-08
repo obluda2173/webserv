680e1e560983ca41b2291ceca3f53986bdf77728
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-04-08 16:40:21 +0200

    map

---
+++ sockets_tryout/client.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 12:09:01 by erian             #+#    #+#             */
+/*   Updated: 2025/04/08 15:53:34 by erian            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+constexpr int PORT = 8080;
+constexpr int BUFFER_SIZE = 1024;
+
+int main() {
+	int sock = 0;
+	struct sockaddr_in serv_addr;
+	char buffer[BUFFER_SIZE] = {0};
+	
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		std::cerr << "socket creation error" << std::endl;
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+		std::cerr << "invalid address / adress not supported" << std::endl;
+		return -1;
+	}
+
+	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+		std::cerr << "connection failed" << std::endl;
+		return -1;
+	}
+
+	std::string msg = "hello from client";
+	send(sock, msg.c_str(), msg.size(), 0);
+	std::cout << "message sent" << std::endl;
+	ssize_t valread = read(sock, buffer, BUFFER_SIZE);
+	std::cout << "received: " << buffer << std::endl;
+	
+	close(sock);
+	return 0;
+}
