3ab7eaf51b8d454586399efc8c69339002ed61fd
Kay Freyer <kfreyer@c1r1s2.42wolfsburg.de>
2025-04-12 18:00:10 +0200

    reading up on more socket_programming

--- tests/unittests/test_server.cpp
+++ tests/unittests/test_server.cpp
@@ -1,10 +1,13 @@
+#include "Logger.h"
 #include "Server.h"
-#include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include <sys/socket.h>
+#include <thread>
 
 class MockLogger : public ILogger {
-public:
-    MOCK_METHOD(void, log, (const std::string& level, const std::string& msg), (override));
+  public:
+    MOCK_METHOD(void, log, (const std::string &level, const std::string &msg), (override));
 };
 
 // defining a Test Fixture: ServerTest
@@ -32,4 +35,16 @@ TEST_F(ServerTest, shutdownTest) {
     svr.start();
     svr.stop();
     EXPECT_FALSE(svr.isRunning());
+}
+
+TEST_F(ServerTest, connectionTests) {
+    Logger logger;
+    testing::internal::CaptureStdout();
+    Server svr(&logger);
+
+    std::thread svr_thread(&Server::start, &svr);
+    svr_thread.join();
+
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    ASSERT_TRUE(client_socket >= 0);
 }
