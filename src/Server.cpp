3cfc0cff8e313f9f548923d8583a03b1c3735630
Kay Freyer <kay.freyer@icloud.com>
2025-04-13 20:38:48 +0200

    fiddling with IP addresses

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,8 +1,8 @@
 #include "Server.h"
+#include <arpa/inet.h>
 #include <iostream>
 #include <netdb.h>
 #include <netinet/in.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -25,11 +25,15 @@ void Server::start() {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
+    int yes = 1;
+    if (setsockopt(_serverfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
     if (bind(_serverfd, (sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind");
         exit(1);
     }
-
     if (listen(_serverfd, 5) < 0) {
         perror("listen");
         exit(1);
@@ -38,17 +42,23 @@ void Server::start() {
     _isRunning = true;
     _logger->log("INFO", "Server started");
 
-    // int clientfd = accept(_serverfd, nullptr, nullptr);
-    // close(clientfd);
+    struct sockaddr_in theirAddr;
+    int addrlen = sizeof(theirAddr);
+    int clientfd = accept(_serverfd, (struct sockaddr *)&theirAddr, (socklen_t *)&addrlen);
+
+    unsigned char *bytes = (unsigned char *)&theirAddr.sin_addr;
+    std::cout << "Incoming connection from IP: " << static_cast<int>(bytes[0]) << '.' << static_cast<int>(bytes[1])
+              << '.' << static_cast<int>(bytes[2]) << '.' << static_cast<int>(bytes[3])
+              << ", Port: " << ntohs(theirAddr.sin_port) << std::endl;
+
+    _logger->log("INFO", "Accepted connection");
+    close(clientfd);
 }
 
 void Server::stop() {
     _logger->log("INFO", "Server is stopping...");
-
-    // this will free the the port
-    int yes = 1;
-    if (setsockopt(_serverfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
-        perror("setsockopt");
+    if (close(_serverfd) == -1) {
+        perror("close");
         exit(1);
     }
     _isRunning = false;
--- tests/unittests/test_server.cpp
+++ tests/unittests/test_server.cpp
@@ -4,7 +4,6 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <netdb.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <thread>
@@ -17,31 +16,39 @@ class MockLogger : public ILogger {
 // defining a Test Fixture: ServerTest
 class ServerTest : public ::testing::Test {};
 
-TEST_F(ServerTest, connectionTests) {
+TEST_F(ServerTest, connectionTest) {
     MockLogger mLogger;
     EXPECT_CALL(mLogger, log("INFO", "Server constructed"));
-    EXPECT_CALL(mLogger, log("INFO", "Server is starting..."));
-    EXPECT_CALL(mLogger, log("INFO", "Server started"));
-    EXPECT_CALL(mLogger, log("INFO", "Server is stopping..."));
-    EXPECT_CALL(mLogger, log("INFO", "Server stopped"));
     Server svr(&mLogger);
 
+    EXPECT_CALL(mLogger, log("INFO", "Server is starting..."));
+    EXPECT_CALL(mLogger, log("INFO", "Server started"));
     std::thread serverThread(&Server::start, &svr);
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
     EXPECT_TRUE(svr.isRunning());
 
-    int i = 0;
-    while (i < 7) {
-        int clientfd = socket(AF_INET, SOCK_STREAM, 0);
-        ASSERT_NE(clientfd, -1) << "Failed to create socket";
+    int clientfd = socket(AF_INET, SOCK_STREAM, 0);
+    ASSERT_NE(clientfd, -1) << "Failed to create socket";
 
-        sockaddr_in server_addr = {};
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(8080);
-        ASSERT_GT(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr), 0);
-        ASSERT_EQ(connect(clientfd, (sockaddr *)&server_addr, sizeof(server_addr)), 0);
-        i++;
-    }
+    sockaddr_in server_addr = {};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    ASSERT_GT(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr), 0);
+
+    EXPECT_CALL(mLogger, log("INFO", "Accepted connection"));
+    ASSERT_EQ(connect(clientfd, (sockaddr *)&server_addr, sizeof(server_addr)), 0);
+
+    struct sockaddr_in local_address;
+    socklen_t address_length = sizeof(local_address);
+    ASSERT_NE(getsockname(clientfd, (struct sockaddr *)&local_address, &address_length), -1);
+    char local_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &local_address.sin_addr, local_ip, sizeof(local_ip));
+    int local_port = ntohs(local_address.sin_port);
+    std::cout << "Local IP: " << local_ip << ", Local Port: " << local_port << std::endl;
+
     serverThread.join();
+
+    EXPECT_CALL(mLogger, log("INFO", "Server is stopping..."));
+    EXPECT_CALL(mLogger, log("INFO", "Server stopped"));
     svr.stop();
 }
