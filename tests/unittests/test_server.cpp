ec08b3108d78f44eb5ced60a66f5c295040d825a
Kay Freyer <kay.freyer@icloud.com>
2025-04-15 11:40:09 +0200

    sockaddr_in outside of while scope

--- tests/unittests/test_server.cpp
+++ tests/unittests/test_server.cpp
@@ -1,6 +1,7 @@
 #include "Server.h"
 #include "test_main.h"
 #include <cstring>
+#include <random>
 
 class MockLogger : public ILogger {
   public:
@@ -17,6 +18,8 @@ TEST_F(ServerTest, connectionTest) {
     MockLogger mLogger;
     EXPECT_CALL(mLogger, log("INFO", "Server constructed"));
     Server svr(&mLogger);
+    sockaddr_in svrAddr;
+    setSvrAddr(svrAddr);
     // startSvr(svr, mLogger);
 
     EXPECT_CALL(mLogger, log("INFO", "Server is starting..."));
@@ -29,12 +32,16 @@ TEST_F(ServerTest, connectionTest) {
     int clientPort;
     clientPort = 8080;
 
+    std::random_device rd;                       // Obtain a random number from hardware
+    std::mt19937 gen(rd());                      // Seed the generator
+    std::uniform_int_distribution<> distr(1, 9); // Define the range
+
     int count = 0;
     while (count++ < 3) {
-        int clientfd = getClientSocket("127.0.0.1", ++clientPort);
+        int randNbr = distr(gen); // Generate the random number
+        clientPort += randNbr;
+        int clientfd = getClientSocket("127.0.0.1", clientPort);
         ASSERT_GT(clientfd, 0) << "getClientSocket failed";
-        sockaddr_in svrAddr;
-        setSvrAddr(svrAddr);
         EXPECT_CALL(mLogger,
                     log("INFO", "Connection accepted from IP: 127.0.0.1, Port: " + std::to_string(clientPort)));
         ASSERT_EQ(connect(clientfd, (sockaddr *)&svrAddr, sizeof(svrAddr)), 0) << "connect: " << strerror(errno);
