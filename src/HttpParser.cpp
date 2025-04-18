22c77236372ba30efbacd7c54129c1fddd07e3ad
Kay Freyer <kfreyer@c1r14s2.42wolfsburg.de>
2025-04-18 14:43:22 +0200

    add TODOs: map, limits, some renames, refactoring

--- src/HttpParser.cpp
+++ src/HttpParser.cpp
@@ -1,8 +1,6 @@
 #include <HttpParser.h>
 
-HttpParser::HttpParser() : _state(STATE_REQUEST_LINE) {
-    _currentRequest.hasBody = false;
-}
+HttpParser::HttpParser() : _state(STATE_REQUEST_LINE) { _currentRequest.hasBody = false; }
 
 HttpParser::~HttpParser() {}
 
@@ -13,11 +11,14 @@ void HttpParser::reset() {
     _currentRequest.hasBody = false;
 }
 
-std::string toLower(const std::string& str);
+std::string toLower(const std::string &str);
 
 void HttpParser::parseBuffer() {
+    // TODO: impose a limit of 256 bytes for the header field (because of hashing, performance)
+    // TODO: have a maximum length (_maxSizeHeader) of 4KB by default and maybe make it configurable (how does nginx do
+    // it?)
     while (_state != STATE_DONE && _state != STATE_ERROR) {
-        if (_buffer.size() > 8000) { // recommended limit
+        if (_buffer.size() > 8000) { // recommended limit TODO: refactor to a private data member (_)
             _state = STATE_ERROR;
             return;
         }
@@ -34,17 +35,22 @@ void HttpParser::parseBuffer() {
         _buffer.erase(0, pos + 2);
 
         if (_state == STATE_REQUEST_LINE) {
+            // TODO: factor out to a function or member function
             std::istringstream iss(line);
             std::string temp;
             iss >> _currentRequest.method >> _currentRequest.uri >> _currentRequest.version >> temp;
-            if (_currentRequest.method.empty() || _currentRequest.uri.empty() || _currentRequest.version.empty() || !temp.empty()) {
+            if (_currentRequest.method.empty() || _currentRequest.uri.empty() || _currentRequest.version.empty() ||
+                !temp.empty()) {
                 _state = STATE_ERROR;
                 return;
             }
             _state = STATE_HEADERS;
         } else if (_state == STATE_HEADERS) {
             if (line.empty()) {
-                for (std::vector<std::pair<std::string, std::string> >::const_iterator it = _currentRequest.headers.begin();
+
+                // TODO: factor out to a function or member function
+                for (std::vector<std::pair<std::string, std::string>>::const_iterator it =
+                         _currentRequest.headers.begin();
                      it != _currentRequest.headers.end(); ++it) {
                     if (it->first == "content-length" || it->first == "transfer-encoding") {
                         _currentRequest.hasBody = true;
@@ -53,6 +59,8 @@ void HttpParser::parseBuffer() {
                 }
                 _state = STATE_DONE;
             } else {
+
+                // TODO: factor out to a function or member function
                 size_t sep = line.find(':');
                 if (sep == std::string::npos) {
                     _state = STATE_ERROR;
@@ -70,35 +78,31 @@ void HttpParser::parseBuffer() {
                     _state = STATE_ERROR;
                     return;
                 }
-                
+
                 _currentRequest.headers.push_back(std::make_pair(key, value));
             }
         }
     }
 }
 
-std::string toLower(const std::string& str) {
+std::string toLower(const std::string &str) {
     std::string result = str;
     for (size_t i = 0; i < result.size(); ++i) {
         if (result[i] >= 'A' && result[i] <= 'Z') {
-            result[i] = result[i] + ('a' - 'A');
+            result[i] = result[i] + ('a' - 'A'); // TODO: use cpp-way
         }
     }
     return result;
 }
 
-void HttpParser::feed(const char* buffer, size_t length) {
+void HttpParser::feed(const char *buffer, size_t length) {
     _buffer.append(buffer, length);
     parseBuffer();
 }
 
-int HttpParser::ready() {
-    return _state == STATE_DONE;
-}
+int HttpParser::ready() { return _state == STATE_DONE; }
 
-int HttpParser::error() {
-    return _state == STATE_ERROR;
-}
+int HttpParser::error() { return _state == STATE_ERROR; }
 
 HttpRequest HttpParser::getRequest() {
     if (_state != STATE_DONE) {
