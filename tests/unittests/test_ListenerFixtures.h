e139e57a389c057f04de79dcf9ec07e656333778
Kay Freyer <kay.freyer@icloud.com>
2025-04-18 20:34:12 +0200

    begin fixture for ListenerTest

--- tests/unittests/test_ListenerFixtures.h
+++ tests/unittests/test_ListenerFixtures.h
@@ -1,19 +1,21 @@
-// #ifndef TEST_LISTENERFIXTURES_H
-// #define TEST_LISTENERFIXTURES_H
+#ifndef TEST_LISTENERFIXTURES_H
+#define TEST_LISTENERFIXTURES_H
 
-// #include "ConnectionHandler.h"
-// #include "EPollManager.h"
-// #include "IConnectionHandler.h"
-// #include <gtest/gtest.h>
+#include "ConnectionHandler.h"
+#include "EPollManager.h"
+#include "IConnectionHandler.h"
+#include "test_mocks.h"
+#include <gtest/gtest.h>
+#include <thread>
 
-// class ListenerTestWithMockLogging : public ::testing::TestWithParam<std::vector<std::string>> {
-//   protected:
-//     MockLogger* _mLogger;
-//     EPollManager* _epollMngr;
-//     IConnectionHandler* _connHdlr;
-//     std::thread _svrThread;
-//     int _openFdsBegin;
-//     std::vector<std::string> _ports;
-// };
+class ListenerTestWithMockLogging : public ::testing::TestWithParam<std::vector<std::string>> {
+  protected:
+    MockLogger* _mLogger;
+    EPollManager* _epollMngr;
+    IConnectionHandler* _connHdlr;
+    std::thread _svrThread;
+    int _openFdsBegin;
+    std::vector<std::string> _ports;
+};
 
-// #endif // TEST_LISTENERFIXTURES_H
+#endif // TEST_LISTENERFIXTURES_H
