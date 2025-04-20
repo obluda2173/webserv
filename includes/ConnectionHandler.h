fe30f33b3cdf43e14343d71c78ac2a93ca21042f
Kay Freyer <kay.freyer@icloud.com>
2025-04-20 08:49:35 +0200

    epollMngr in ConnectionHandler is reference

--- includes/ConnectionHandler.h
+++ includes/ConnectionHandler.h
@@ -20,13 +20,13 @@ class ConnectionHandler : public IConnectionHandler {
   private:
     std::map<int, ConnectionInfo> _connections;
     ILogger* _logger;
-    EPollManager* _epollMngr;
+    EPollManager& _epollMngr;
     void _addClientConnection(int conn, struct sockaddr* theirAddr);
     void _removeClientConnection(ConnectionInfo connInfo);
     void _acceptNewConnection(int socketfd);
 
   public:
-    ConnectionHandler(ILogger*, EPollManager*);
+    ConnectionHandler(ILogger*, EPollManager&);
     ~ConnectionHandler(void);
     void handleConnection(int conn);
 };
