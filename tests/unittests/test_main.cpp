b97aea6fe357c74a51b39c73481a992782a091c8
Kay Freyer <kay.freyer@icloud.com>
2025-04-20 08:21:59 +0200

    tests run through fairly stable

--- tests/unittests/test_main.cpp
+++ tests/unittests/test_main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
     // testing::GTEST_FLAG(filter) = "*ListenerTestWithMockLogging*";
     // testing::GTEST_FLAG(filter) = "*ListenerTestWo*";
-    testing::GTEST_FLAG(filter) = "*ListenerTest*";
+    // testing::GTEST_FLAG(filter) = "*ListenerTest*";
     // testing::GTEST_FLAG(filter) = "ListenerTest.closingAConnection";
     // testing::GTEST_FLAG(filter) = "ListenerTest.closingAConnection:ListenerTest.multiplePortsTestWoLogging";
     // testing::GTEST_FLAG(filter) = "ServerWithMockLoggerParametrizedPortTest";
