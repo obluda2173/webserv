343fb5679c4c9d339d6cd4d6b8c5ab6098eb9d10
Kay Freyer <kfreyer@c2r8s8.42wolfsburg.de>
2025-04-22 19:03:08 +0200

    beginning of some new test

--- tests/unittests/test_ConnectionHandler.cpp
+++ tests/unittests/test_ConnectionHandler.cpp
@@ -41,3 +41,26 @@ TEST_F(ConnectionHdlrTest, pingTest) {
                                "pong";
     EXPECT_STREQ(buffer, wantResponse.c_str());
 }
+
+// TEST_F(ConnectionHdlrTest, incompleteRequest) {
+//     char buffer[1024];
+//     std::string msg = "GET /ping HTT";
+
+//     // send msg
+//     send(_clientfd, msg.c_str(), msg.length(), 0);
+//     _connHdlr->handleConnection(_conn, READY_TO_READ);
+
+//     // check that nothing is sent back yet
+//     recv(_clientfd, buffer, 1024, 0);
+//     ASSERT_EQ(errno, EWOULDBLOCK);
+
+//     // next time around the response is sent back
+//     _connHdlr->handleConnection(_conn, READY_TO_WRITE);
+//     ssize_t r = recv(_clientfd, buffer, 1024, 0);
+//     buffer[r] = '\0';
+//     std::string wantResponse = "HTTP/1.1 200 OK\r\n"
+//                                "Content-Length: 4\r\n"
+//                                "\r\n"
+//                                "pong";
+//     EXPECT_STREQ(buffer, wantResponse.c_str());
+// }
