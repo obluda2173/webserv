f89cfd18e3db2662d9e6e56a0ddccd4a0855c765
Kay Freyer <kfreyer@c2r13s4.42wolfsburg.de>
2025-04-24 17:31:26 +0200

    actuallly you need to do it with pipes

--- tests/unittests/test_IONotifier.cpp
+++ tests/unittests/test_IONotifier.cpp
@@ -1,6 +1,7 @@
 #include "EpollIONotifier.h"
 #include "IIONotifier.h"
 #include "test_stubs.h"
+#include <cstring>
 #include <gtest/gtest.h>
 #include <netdb.h>
 #include <sys/socket.h>
@@ -15,6 +16,31 @@ TEST(TestIONotifier, testBrokenConnection) {
     int fd = newListeningSocket(svrAddrInfo, 5);
     freeaddrinfo(svrAddrInfo);
 
+    ioNotifier->add(fd, READY_TO_READ);
+
+    shutdown(fd, SHUT_RDWR);
+
+    int fds;
+    e_notif notifs;
+    ioNotifier->wait(&fds, &notifs);
+
+    ASSERT_EQ(BROKEN_CONNECTION, notifs);
+
+    delete ioNotifier;
+    delete logger;
+}
+
+TEST(TestIONotifier, testBeingAbleToReadButBrokenConnection) {
+    ILogger* logger = new StubLogger();
+    IIONotifier* ioNotifier = new EpollIONotifier(*logger);
+
+    struct addrinfo* svrAddrInfo;
+    getAddrInfoHelper(NULL, "8080", AF_INET, &svrAddrInfo);
+    int fd = newListeningSocket(svrAddrInfo, 5);
+    freeaddrinfo(svrAddrInfo);
+
+    std::string msg = "A message written into fd";
+    send;
     ioNotifier->add(fd, READY_TO_READ);
 
     shutdown(fd, SHUT_RDWR);
