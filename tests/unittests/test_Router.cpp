f16a3d8e9752bf0610aa8d9010b2705fedc64680
Kay Freyer <kay.freyer@icloud.com>
2025-05-02 15:05:09 +0200

    green

--- tests/unittests/test_Router.cpp
+++ tests/unittests/test_Router.cpp
@@ -35,9 +35,15 @@ TEST_P(RouterTest, testWithConfigParsing) {
 
     IConfigParser* cfgPrsr = new ConfigParser("./tests/unittests/test_configs/config1.conf");
     cfgPrsr->getServersConfig();
-    Router r = newRouter(cfgPrsr->getServersConfig(), new StubHandler("GET"), new StubHandler("POST"), new StubHandler("DELETE"));
+    Router r = newRouter(cfgPrsr->getServersConfig(), new StubHandler("GET"), new StubHandler("POST"),
+                         new StubHandler("DELETE"));
     Route gotRoute = r.match(request);
     EXPECT_EQ(wantCfg.root, gotRoute.cfg.root);
+    // for (size_t i = 0; i < wantCfg.index.size(); i++) {
+    //     EXPECT_EQ(wantCfg.index[i], gotRoute.cfg.index[i]);
+    // }
+    // EXPECT_EQ(wantCfg.index.size(), gotRoute.cfg.index.size());
+    EXPECT_EQ(wantCfg.errorPage.size(), gotRoute.cfg.errorPage.size());
     EXPECT_EQ(wantHdlrs.size(), gotRoute.hdlrs.size());
     delete cfgPrsr;
 }
@@ -49,13 +55,14 @@ INSTANTIATE_TEST_SUITE_P(
         //                  "/data/scripts/css/scripts/script.js", "GET"},
         // RouterTestParams{HttpRequest{"POST", "/css/scripts/script.js", "HTTP/1.1", {{"host", "example.com"}}},
         //                  "/data/scripts/css/scripts/script.js", "POST"},
-        // RouterTestParams{HttpRequest{"GET", "/", "HTTP/1.1", {{"host", "test3.com"}}}, "/test3/www/html/", "GET"},
-        // RouterTestParams{HttpRequest{"POST", "/index.html", "HTTP/1.1", {{"host", "example.com"}}}, "",
-        //                  "ERROR"}, // Fuzzy test but it pointed me to an issue with servers that don't have a root
+        // RouterTestParams{HttpRequest{"GET", "/", "HTTP/1.1", {{"host", "test3.com"}}}, "/test3/www/html/",
+        // "GET"}, RouterTestParams{HttpRequest{"POST", "/index.html", "HTTP/1.1", {{"host", "example.com"}}}, "",
+        //                  "ERROR"}, // Fuzzy test but it pointed me to an issue with servers that don't have a
+        //                  root
         // RouterTestParams{HttpRequest{"POST", "/js/something", "HTTP/1.1", {{"host", "test.com"}}}, "", "ERROR"},
         // RouterTestParams{HttpRequest{"POST", "/js/", "HTTP/1.1", {{"host", "test.com"}}}, "", "ERROR"},
-        // RouterTestParams{HttpRequest{"POST", "/", "HTTP/1.1", {{"host", "test.com"}}}, "/var/www/secure/", "POST"},
-        // RouterTestParams{HttpRequest{"POST", "/", "HTTP/1.1", {{"host", "test2.com"}}}, "", "ERROR"},
+        // RouterTestParams{HttpRequest{"POST", "/", "HTTP/1.1", {{"host", "test.com"}}}, "/var/www/secure/",
+        // "POST"}, RouterTestParams{HttpRequest{"POST", "/", "HTTP/1.1", {{"host", "test2.com"}}}, "", "ERROR"},
         // RouterTestParams{HttpRequest{"GET", "/css/styles/", "HTTP/1.1", {{"host", "test.com"}}},
         //                  "/data/static/css/styles/", "GET"},
         // RouterTestParams{HttpRequest{"GET", "/images/", "HTTP/1.1", {{"host", "test.com"}}}, "/data2/images/",
@@ -71,18 +78,30 @@ INSTANTIATE_TEST_SUITE_P(
         //                  "/data/images/themes/", "GET"},
         // RouterTestParams{HttpRequest{"GET", "/css/themes/", "HTTP/1.1", {{"host", "example.com"}}},
         //                  "/data/static/css/themes/", "GET"},
-        // RouterTestParams{HttpRequest{"GET", "/", "HTTP/1.1", {{"host", "unknown.com"}}}, "/var/www/html/", "GET"},
-        // RouterTestParams{HttpRequest{"GET", "/css", "HTTP/1.1", {{"host", "example.com"}}}, "/var/www/html/css",
-        // "GET"},
-        RouterTestParams{HttpRequest{"GET", "/js/", "HTTP/1.1", {{"host", "test.com"}}}, {"GET"}, {"/data/scripts", {}, {}, 1024, false}},
+        // RouterTestParams{HttpRequest{"GET", "/", "HTTP/1.1", {{"host", "unknown.com"}}}, "/var/www/html/",
+        // "GET"}, RouterTestParams{HttpRequest{"GET", "/css", "HTTP/1.1", {{"host", "example.com"}}},
+        // "/var/www/html/css", "GET"},
+        RouterTestParams{HttpRequest{"GET", "/js/", "HTTP/1.1", {{"host", "test.com"}}},
+                         {"GET"},
+                         {"/data/scripts", {}, {}, 1024, false}},
         RouterTestParams{HttpRequest{"GET", "/keys/", "HTTP/1.1", {{"host", "test.com"}}},
                          {"GET", "POST", "DELETE"},
                          {"/var/www/secure", {"index.html", "index.htm"}, {}, 1024, false}},
-        RouterTestParams{HttpRequest{"GET", "/images/", "HTTP/1.1", {{"host", "example.com"}}}, {"GET", "POST", "DELETE"}, {"/data", {}, {}, 1024, false}},
-        RouterTestParams{HttpRequest{"GET", "/css/", "HTTP/1.1", {{"host", "example.com"}}}, {"GET", "POST"}, {"/data/static", {}, {}, 1024, false}},
-        RouterTestParams{HttpRequest{"GET", "/index.html", "HTTP/1.1", {{"host", "example.com"}}}, {"GET"}, {"/var/www/html", {}, {}, 1024, false}},
-        RouterTestParams{HttpRequest{"GET", "/", "HTTP/1.1", {{"host", "example.com"}}}, {"GET"}, {"/var/www/html", {}, {}, 1024, false}},
+        RouterTestParams{HttpRequest{"GET", "/images/", "HTTP/1.1", {{"host", "example.com"}}},
+                         {"GET", "POST", "DELETE"},
+                         {"/data", {}, {}, 1024, false}},
+        RouterTestParams{HttpRequest{"GET", "/css/", "HTTP/1.1", {{"host", "example.com"}}},
+                         {"GET", "POST"},
+                         {"/data/static", {}, {}, 1024, false}},
+        RouterTestParams{HttpRequest{"GET", "/index.html", "HTTP/1.1", {{"host", "example.com"}}},
+                         {"GET"},
+                         {"/var/www/html", {}, {}, 1024, false}},
+        RouterTestParams{HttpRequest{"GET", "/", "HTTP/1.1", {{"host", "example.com"}}},
+                         {"GET"},
+                         {"/var/www/html", {}, {}, 1024, false}},
         RouterTestParams{HttpRequest{"GET", "/", "HTTP/1.1", {{"host", "test.com"}}},
                          {"GET", "POST", "DELETE"},
                          {"/var/www/secure", {"index.html", "index.htm"}, {}, 1024, false}},
-        RouterTestParams{HttpRequest{"GET", "/", "HTTP/1.1", {{"host", "test2.com"}}}, {"GET"}, {"/usr/share/nginx/html", {}, {}, 1024, false}}));
+        RouterTestParams{HttpRequest{"GET", "/", "HTTP/1.1", {{"host", "test2.com"}}},
+                         {"GET"},
+                         {"/usr/share/nginx/html", {}, {}, 1024, false}}));
