4340f46fc9f385377a39a893af38bbe77578e6fd
Kay Freyer <kfreyer@c1r7s4.42wolfsburg.de>
2025-05-11 15:30:37 +0200

    writing main

--- run/main.cpp
+++ run/main.cpp
@@ -1,18 +1,25 @@
+#include "ConfigParser.h"
 #include "ConnectionHandler.h"
 #include "EpollIONotifier.h"
+#include "GetHandler.h"
 #include "Logger.h"
+#include "Router.h"
 #include "ServerBuilder.h"
 
 int main() {
+
+    IConfigParser* cfgPrsr = new ConfigParser("tests/end_to_end_tests/Config1/config1.cfg");
     Logger* logger = new Logger();
     EpollIONotifier* ioNotifier = new EpollIONotifier(*logger);
-    ConnectionHandler* connHdlr = new ConnectionHandler(*logger, *ioNotifier);
+
+    std::map<std::string, IHandler*> hdlrs = {{"GET", new GetHandler()}};
+    Router router = newRouter(cfgPrsr->getServersConfig(), hdlrs);
+    ConnectionHandler* connHdlr = new ConnectionHandler(&router, *logger, *ioNotifier);
 
     Server* svr = ServerBuilder().setLogger(logger).setIONotifier(ioNotifier).setConnHdlr(connHdlr).build();
 
     std::vector<std::string> ports;
     ports.push_back("8080");
-    ports.push_back("8081");
 
     svr->start(ports);
 
