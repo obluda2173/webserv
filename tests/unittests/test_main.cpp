e73e8aa5530dfa08fc388f37d946e512f49991a7
Diqing Yao <dyao@c1r7s3.42wolfsburg.de>
2025-05-14 13:07:23 +0200

    red test (filePathNotExist)

--- tests/unittests/test_main.cpp
+++ tests/unittests/test_main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
     // testing::GTEST_FLAG(filter) = "-*ResponseWriterTest*";
     // testing::GTEST_FLAG(filter) = "-*TestHttpParser*";
     // testing::GTEST_FLAG(filter) = "-*ListenerTest*:*ServerTest*:*TestHttpParser*";
-    testing::GTEST_FLAG(filter) = "-*ListenerTest*:*ServerTest*";
+    // testing::GTEST_FLAG(filter) = "-*ListenerTest*:*ServerTest*";
     // testing::GTEST_FLAG(filter) = "-*ServerTest*";
     // testing::GTEST_FLAG(filter) = "-*ConnHdlrTestOneConnection.TestPersistenceSendInBatches*";
     // testing::GTEST_FLAG(filter) = "-*sendMsgsAsync/ConnectionHdlrTestOneConnection.TestPersistenceSendInBatches*";
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     // testing::GTEST_FLAG(filter) = "*CgiHandlerTest*";
     // testing::GTEST_FLAG(filter) = "*HttpParserTest*";
     // testing::GTEST_FLAG(filter) = "*DeleteHandler*";
-    // testing::GTEST_FLAG(filter) = "*UploadHdlr*";
+    testing::GTEST_FLAG(filter) = "*UploadHdlr*";
     // testing::GTEST_FLAG(filter) = "*ConnHdlrTestUpload*";
     // testing::GTEST_FLAG(filter) = "*ConnectionTestResend*";
     // testing::GTEST_FLAG(filter) = "*ConnHdlrTestWithBigBody*";
