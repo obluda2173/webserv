d35134cc029e93fc5fc87fc46f5617285b5886ac
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-05-14 17:29:24 +0200

    not needed

--- src/ConnectionHandler.cpp
+++ src/ConnectionHandler.cpp
@@ -38,13 +38,9 @@ void ConnectionHandler::_updateNotifier(Connection* conn) {
     case Connection::Reset:
         _ioNotifier.modify(connfd, READY_TO_READ);
         break;
-    case Connection::HandlingCgi: {
-        int pipeFd = conn->cgiCtx.cgiPipeFd;
-            if (pipeFd != -1) {
-                _ioNotifier.modify(pipeFd, READY_TO_READ);
-            }
-            break;
-        }
+    case Connection::HandlingCgi:
+        _ioNotifier.modify(conn->cgiCtx.cgiPipeFd, READY_TO_READ);
+        break;
     }
 }
 
