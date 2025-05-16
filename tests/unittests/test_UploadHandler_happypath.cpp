2a591d0fc913367a58cfd0c57fe915e1c6a3c4fe
DiqingJunYao <tiny-rice@outlook.com>
2025-05-16 16:18:22 +0200

    make a new green test for the file that we should replace with, and it will not be touched until everything finish

--- tests/unittests/test_UploadHandler_happypath.cpp
+++ tests/unittests/test_UploadHandler_happypath.cpp
@@ -142,8 +142,8 @@ INSTANTIATE_TEST_SUITE_P(first, UploadHdlrTest,
                                          UploadHandlerTestParams{
                                              {"1.txt", "2.txt"}, {1000, 1000}, {444, 555}, 10, 20000}));
 
-TEST(UploadHdlrTest, testTempFileExist) {
-    std::string filename = "existing.txt";
+TEST(UploadHdlrTest, testTempFileExistFileNotExist) {
+    std::string filename = "notExisting.txt";
     size_t pos = 0;
     size_t batchSize = 10;
     
@@ -157,11 +157,17 @@ TEST(UploadHdlrTest, testTempFileExist) {
             conn->setReadBuf(body.substr(pos, batchSize));
             uploadHdlr->handle(conn, conn->_request, {ROOT, {}, {}, 10000, false, {}});
             struct stat statStruct;
-            bool exists = !stat((ROOT + PREFIX + filename + ".temp").c_str(), &statStruct);
-            if (pos < contentLength - batchSize)
-                EXPECT_EQ(exists, true);
-            else
-                EXPECT_EQ(exists, false);
+            bool fileExists = !stat((ROOT + PREFIX + filename).c_str(), &statStruct);
+            
+            bool tempExists = !stat((ROOT + PREFIX + filename + ".temp").c_str(), &statStruct);
+            if (pos < contentLength - batchSize) {
+                EXPECT_EQ(fileExists, false);
+                EXPECT_EQ(tempExists, true);
+            }
+            else {
+                EXPECT_EQ(fileExists, true);
+                EXPECT_EQ(tempExists, false);
+            }
         }
         pos += batchSize;
     }
@@ -173,6 +179,55 @@ TEST(UploadHdlrTest, testTempFileExist) {
     EXPECT_EQ(body, gotFile1);
     EXPECT_EQ(201, resp.statusCode);
     EXPECT_EQ("Created", resp.statusMessage);
+
+    removeFile(ROOT + PREFIX + filename);
+    delete uploadHdlr;
+}
+
+TEST(UploadHdlrTest, testTempFileExistFileExist) {
+    std::string filename = "existing.txt";
+    std::ofstream file(ROOT + PREFIX + filename);
+    ASSERT_TRUE(file.is_open());
+    file << "some content";
+    file.close();
+
+    size_t pos = 0;
+    size_t batchSize = 10;
+    
+    size_t contentLength = 100;
+    std::string body = getRandomString(contentLength);
+    Connection* conn = setupConnWithContentLength(filename, contentLength);
+
+    IHandler* uploadHdlr = new UploadHandler();
+    while (pos < contentLength) {
+        if (pos <= contentLength) {
+            conn->setReadBuf(body.substr(pos, batchSize));
+            uploadHdlr->handle(conn, conn->_request, {ROOT, {}, {}, 10000, false, {}});
+            struct stat statStruct;
+            bool fileExists = !stat((ROOT + PREFIX + filename).c_str(), &statStruct);
+            
+            bool tempExists = !stat((ROOT + PREFIX + filename + ".temp").c_str(), &statStruct);
+            if (pos < contentLength - batchSize) {
+                std::string gotFile2 = getFileContents(ROOT + PREFIX + filename);
+                EXPECT_EQ("some content", gotFile2);
+                EXPECT_EQ(fileExists, true);
+                EXPECT_EQ(tempExists, true);
+            }
+            else {
+                EXPECT_EQ(fileExists, true);
+                EXPECT_EQ(tempExists, false);
+            }
+        }
+        pos += batchSize;
+    }
+
+    HttpResponse resp = conn->_response;
+    delete conn; // need to delete conn to close the file and write to disk
+    std::string gotFile1 = getFileContents(ROOT + PREFIX + filename);
+    EXPECT_EQ(body.length(), gotFile1.length());
+    EXPECT_EQ(body, gotFile1);
+    EXPECT_EQ(200, resp.statusCode);
+    EXPECT_EQ("OK", resp.statusMessage);
 
     removeFile(ROOT + PREFIX + filename);
     delete uploadHdlr;
