e566139559b80abb0ffa1433f9634652cfdbc57a
Kay Freyer <kfreyer@c1r6s4.42wolfsburg.de>
2025-05-20 15:57:13 +0200

    unfinished test

--- tests/unittests/test_BodyParser.cpp
+++ tests/unittests/test_BodyParser.cpp
@@ -34,3 +34,34 @@ TEST(BodyParserTest, firstTest) {
     delete conn;
     delete bodyPrsr;
 }
+
+TEST(BodyParserTestContentLength, concurrently) {
+    BodyParser* bodyPrsr = new BodyParser();
+
+    Connection* conn = new Connection({}, -1, 0, NULL, NULL);
+    int contentLength = 9876;
+
+    conn->_request.method = "";
+    conn->_request.uri = "";
+    conn->_request.version = "";
+    conn->_request.headers["content-length"] = std::to_string(contentLength);
+    conn->setState(Connection::Handling);
+    conn->_bodyFinished = false;
+
+    std::string body = getRandomString(contentLength);
+    std::string bytes = body + getRandomString(10000 - contentLength);
+
+    size_t pos = 0;
+    while (pos < bytes.length()) {
+        int chunkSize = getRandomNumber(10, 50);
+        std::string bytesChunk = bytes.substr(pos, chunkSize);
+        pos += chunkSize;
+        conn->_readBuf.assign(bytesChunk);
+
+        bodyPrsr->parse(conn);
+
+        if (pos <= body.length()) {
+            EXPECT_EQ(conn->_tempBody, bytesChunk);
+        }
+    }
+}
