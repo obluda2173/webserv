47fe9ea34fce599c846a10c07d3c7a224232da24
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-05-21 18:18:50 +0200

    working test

--- tests/unittests/test_CgiHandler_with_body.cpp
+++ tests/unittests/test_CgiHandler_with_body.cpp
@@ -1,120 +1,116 @@
-// #include "CgiHandler.h"
-// #include "BodyParser.h"
-// #include "Connection.h"
-// #include "HttpRequest.h"
-// #include "test_main.h"
-// #include "gtest/gtest.h"
-// #include <gtest/gtest.h>
-// #include <thread>
+#include "CgiHandler.h"
+#include "BodyParser.h"
+#include "Connection.h"
+#include "HttpRequest.h"
+#include "test_main.h"
+#include "gtest/gtest.h"
+#include <gtest/gtest.h>
+#include <thread>
 
-// struct CgiTestParams {
-//     std::string testName;
-//     std::string scriptName;
-//     std::string postBody;
-//     std::string contentType;
-//     int expectedStatus;
-//     std::string wantOutput;
-// };
+struct CgiTestParams2 {
+    std::string testName;
+    std::string scriptName;
+    std::string postBody;
+    std::string contentType;
+    int expectedStatus;
+    std::string wantOutput;
+};
 
-// class CgiPostHandlerTest : public testing::TestWithParam<CgiTestParams> {
-//     protected:
-//         void simulateBodyParsing(Connection* conn, const std::string& body) {
-//             BodyParser bodyParser;
-//             conn->_request.headers["content-length"] = std::to_string(body.size());
-//             conn->_readBuf.assign(body);
-//             conn->_bodyFinished = false;
+class CgiPostHandlerTest : public testing::TestWithParam<CgiTestParams2> {
+    protected:
+        void simulateBodyParsing(Connection* conn, const std::string& body) {
+            BodyParser bodyParser;
+            conn->_request.headers["content-length"] = std::to_string(body.size());
+            conn->_readBuf.assign(body);
+            conn->_bodyFinished = false;
             
-//             while (!conn->_bodyFinished) {
-//                 bodyParser.parse(conn);
-//             }
-//         }
-// };
+            while (!conn->_bodyFinished) {
+                bodyParser.parse(conn);
+            }
+        }
+};
 
-// TEST_P(CgiPostHandlerTest, HandlesPostRequests) {
-//     const CgiTestParams& params = GetParam();
-//     RouteConfig cfg = {
-//         "tests/unittests/test_cgi/scripts",
-//         {}, 
-//         {},
-//         1000000,
-//         false,
-//         {{"php", "/usr/bin/php-cgi"}, {"py", "/usr/bin/python3"}}
-//     };
+TEST_P(CgiPostHandlerTest, HandlesPostRequests) {
+    const CgiTestParams2& params = GetParam();
+    RouteConfig cfg = {
+        "tests/unittests/test_cgi/scripts",
+        {}, 
+        {},
+        1000000,
+        false,
+        {{"php", "/usr/bin/php-cgi"}, {"py", "/usr/bin/python3"}}
+    };
 
-//     HttpRequest req;
-//     req.method = "POST";
-//     req.uri = "/" + params.scriptName;
-//     req.headers["content-type"] = params.contentType;
+    HttpRequest req = HttpRequest();
+    req.method = "POST";
+    req.uri = "/" + params.scriptName;
+    req.headers["content-type"] = params.contentType;
     
-//     CgiHandler cgiHandler;
-//     Connection* conn = new Connection({}, -1, "", NULL, NULL);
-//     conn->setState(Connection::Handling);
+    CgiHandler cgiHandler;
+    Connection* conn = new Connection({}, -1, "", NULL, NULL);
+    conn->setState(Connection::Handling);
 
     
-//     // Simulate body parsing phase
-//     simulateBodyParsing(conn, params.postBody);
+    simulateBodyParsing(conn, params.postBody);
     
-//     // Verify body parsing completed successfully
-//     ASSERT_TRUE(conn->_bodyFinished);
-//     ASSERT_EQ(conn->_tempBody, params.postBody);
+    ASSERT_TRUE(conn->_bodyFinished);
+    ASSERT_EQ(conn->_tempBody, params.postBody);
 
-//     // Execute CGI handling
-//     cgiHandler.handle(conn, req, cfg);
+    cgiHandler.handle(conn, req, cfg);
 
-//     // Process CGI execution
-//     auto start = std::chrono::steady_clock::now();
-//     while (conn->getState() != Connection::SendResponse) {
-//         if (std::chrono::steady_clock::now() - start > std::chrono::seconds(5)) {
-//             FAIL() << "CGI processing timed out";
-//         }
-//         // std::cout << conn->getState() << "\n";
+    auto start = std::chrono::steady_clock::now();
+    while (conn->getState() != Connection::SendResponse) {
+        if (std::chrono::steady_clock::now() - start > std::chrono::seconds(5)) {
+            FAIL() << "CGI processing timed out";
+        }
         
-//         if (conn->getState() == Connection::Handling || conn->getState() == Connection::HandlingCgi) {
-//             cgiHandler.handleCgiProcess(conn);
-//         }
-//         std::this_thread::sleep_for(std::chrono::milliseconds(10));
-//     }
+        if (conn->getState() == Connection::Handling || conn->getState() == Connection::HandlingCgi) {
+            cgiHandler.handleCgiProcess(conn);
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
 
-//     // Validate results
-//     ASSERT_EQ(conn->_response.statusCode, params.expectedStatus);
-//     ASSERT_EQ(getOutput(conn), params.wantOutput);
+    ASSERT_EQ(conn->_response.statusCode, params.expectedStatus);
+    std::string gotOutput = getOutput(conn);
+    ASSERT_EQ(gotOutput, params.wantOutput);
 
-//     delete conn;
-// }
+    delete conn;
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    CgiPostTests,
+    CgiPostHandlerTest,
+    testing::Values(
+        CgiTestParams2{
+            CgiTestParams2{
+                "FormData",
+                "form_processor.py",
+                "name=kay&hobby=coding",
+                "application/x-www-form-urlencoded",
+                200,
+                "name kay\nhobby coding\n" 
+            },
+        }
+    ),
+    [](const testing::TestParamInfo<CgiTestParams2>& info) {
+        return info.param.testName;
+    }
+);
 
 // INSTANTIATE_TEST_SUITE_P(
-//     CgiPostTests,
+//     QueryParamTests,
 //     CgiPostHandlerTest,
 //     testing::Values(
-//         // Basic POST with URL-encoded data
-//         CgiTestParams{
-//             "FormData",
-//             "form_processor.py",
-//             "name=kay&hobby=coding",
-//             "application/x-www-form-urlencoded",
+//         CgiTestParams2{
+//             "GetWithParams",
+//             "MqueryParams.py",
+//             "",  // Empty body for GET
+//             "",
 //             200,
-//             "name: kay\nhobby: coding"
+//             "name kay\nhobby coding, running\n"
 //         }
 //     ),
-//     [](const testing::TestParamInfo<CgiTestParams>& info) {
+//     [](const testing::TestParamInfo<CgiTestParams2>& info) {
 //         return info.param.testName;
 //     }
 // );
-
-// // INSTANTIATE_TEST_SUITE_P(
-// //     QueryParamTests,
-// //     CgiPostHandlerTest,
-// //     testing::Values(
-// //         CgiTestParams{
-// //             "GetWithParams",
-// //             "MqueryParams.py",
-// //             "",  // Empty body for GET
-// //             "",
-// //             200,
-// //             "name kay\nhobby coding, running\n"
-// //         }
-// //     ),
-// //     [](const testing::TestParamInfo<CgiTestParams>& info) {
-// //         return info.param.testName;
-// //     }
-// // );
--- tests/unittests/test_cgi/scripts/form_processor.py
+++ tests/unittests/test_cgi/scripts/form_processor.py
@@ -1,27 +1,13 @@
 #!/usr/bin/env python3
-import os
-import sys
-from urllib.parse import parse_qs
+import cgi, cgitb 
 
-def main():
-    # Get content length from environment
-    content_length = int(os.environ.get('CONTENT_LENGTH', 0))
-    
-    # Read POST data from stdin
-    post_data = sys.stdin.read(content_length)
-    
-    # Parse URL-encoded form data
-    parsed_data = parse_qs(post_data)
-    
-    # Prepare response
-    print("Content-Type: text/plain")
-    print()
-    
-    # Format output as specified in test case
-    for key in sorted(parsed_data.keys()):
-        values = parsed_data[key]
-        # Join multiple values with comma (handles arrays)
-        print(f"{key}: {', '.join(values)}")
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
 
-if __name__ == "__main__":
-    main()
+# Get data from fields
+name = form.getvalue('name')
+hobby  = form.getvalue('hobby')
+
+print("Content-type:text/html\r\n")
+print("name " + name)
+print("hobby " + hobby)
