f827608c17c75643c33892cb14beb646121c0773
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-05-22 12:20:01 +0200

    fixed no server_name in config

--- tests/unittests/test_main.cpp
+++ tests/unittests/test_main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]) {
     // testing::GTEST_FLAG(filter) = "*ConnHdlrTestTestRouting*";
     // testing::GTEST_FLAG(filter) = "*ConnHdlrTestStubUploadHdlr*";
     // testing::GTEST_FLAG(filter) = "*ConnHdlrTestUpload*";
-    testing::GTEST_FLAG(filter) = "*Cgi*";
+    // testing::GTEST_FLAG(filter) = "*Cgi*";
     // testing::GTEST_FLAG(filter) = "*HttpParserTest*";
     // testing::GTEST_FLAG(filter) = "*DeleteHandler*";
     // testing::GTEST_FLAG(filter) = "*UploadHdlr*";
@@ -72,6 +72,6 @@ int main(int argc, char* argv[]) {
     // testing::GTEST_FLAG(filter) = "*ServerTest*"; 
     // testing::GTEST_FLAG(filter) = "*HttpParser*";
     // testing::GTEST_FLAG(filter) = "*HandlerTest*";
-    // testing::GTEST_FLAG(filter) = "*ServerConfig*";
+    testing::GTEST_FLAG(filter) = "*ServerConfig*";
     return RUN_ALL_TESTS();
 }
