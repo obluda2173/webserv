8deeb40989cbf0c848edd5e2f58aa5da708bc3ea
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-05-28 15:54:04 +0200

    no, no, no, mr core dump. you wont go to your family. you want go to yebaniy exception

--- run/main.cpp
+++ run/main.cpp
@@ -33,24 +33,30 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    std::string filename = std::string(argv[1]);
-    IConfigParser* cfgPrsr = new ConfigParser(filename);
-    std::vector< ServerConfig > svrCfgs = cfgPrsr->getServersConfig();
-    delete cfgPrsr;
+    try {
+        std::string filename = std::string(argv[1]);
+        IConfigParser* cfgPrsr = new ConfigParser(filename);
+        std::vector< ServerConfig > svrCfgs = cfgPrsr->getServersConfig();
+        delete cfgPrsr;
 
-    mustTranslateToRealIps(svrCfgs);
-    std::map< std::string, IRouter* > routers = buildRouters(svrCfgs);
+        mustTranslateToRealIps(svrCfgs);
+        std::map< std::string, IRouter* > routers = buildRouters(svrCfgs);
 
-    Logger* logger = new Logger();
-    EpollIONotifier* ioNotifier = new EpollIONotifier(*logger);
-    ConnectionHandler* connHdlr = new ConnectionHandler(routers, *logger, *ioNotifier);
-    Server* svr = ServerBuilder().setLogger(logger).setIONotifier(ioNotifier).setConnHdlr(connHdlr).build();
+        Logger* logger = new Logger();
+        EpollIONotifier* ioNotifier = new EpollIONotifier(*logger);
+        ConnectionHandler* connHdlr = new ConnectionHandler(routers, *logger, *ioNotifier);
+        Server* svr = ServerBuilder().setLogger(logger).setIONotifier(ioNotifier).setConnHdlr(connHdlr).build();
 
-    std::set< std::pair< std::string, std::string > > addrAndPorts = fillAddrAndPorts(svrCfgs);
-
-    svr->start(addrAndPorts, &g_running);
-
-    svr->stop();
-    delete svr;
+        std::set< std::pair< std::string, std::string > > addrAndPorts = fillAddrAndPorts(svrCfgs);
+        svr->start(addrAndPorts, &g_running);
+        svr->stop();
+        delete svr;
+    } catch (const std::exception& e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cerr << "Unknown exception caught" << std::endl;
+        return 1;
+    }
     return 0;
 }
